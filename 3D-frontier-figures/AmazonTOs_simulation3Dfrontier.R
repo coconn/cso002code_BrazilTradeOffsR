243ed5fb894b1681db9d8c961915f2a2f71cf0de
Christine O'Connell <coconn@umn.edu>
2015-03-11 21:42:53 -0500

    new eff frontier figures

--- 3D-frontier-figures/AmazonTOs_simulation3Dfrontier.R
+++ 3D-frontier-figures/AmazonTOs_simulation3Dfrontier.R
@@ -30,10 +30,69 @@ dat <- read.table("~/Documents/GITHUB/cso002code_BrazilTradeOffsR/Fig 3 PNAS ver
 dat <- subset(dat, dat$SimulationFactor == "AllLand")
 # attach(dat)
 
+# where to save things
+dirsave="~/Documents/GITHUB/cso002code_BrazilTradeOffsR/3D-frontier-figures/EfficiencyFrontiers/"
+setwd(dirsave)
+
 
 ##### SECOND: Scatter with third dim as color
 
+## carbon and biodiv on the axes
+
+# efficiency frontier, no limit on third variable
+png(file = "eff-frontier-1.png",width=10,height=10,units="in",res=150)
+# make plot
 ggplot(dat, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red")
+dev.off()
+
+# reverse axes so shape is concave
+png(file = "eff-frontier-2.png",width=10,height=10,units="in",res=150)
+# make plot
+ggplot(dat, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+dev.off()
+
+## carbon and regclim on the axes
+
+# efficiency frontier, no limit on third variable
+png(file = "eff-frontier-3.png",width=10,height=10,units="in",res=150)
+# make plot
+ggplot(dat, aes(x = -EmittedC, y = ClimRegIndexAvgImpac, colour=HabAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red")
+dev.off()
+
+# reverse axes so shape is concave
+png(file = "eff-frontier-4.png",width=10,height=10,units="in",res=150)
+# make plot
+ggplot(dat, aes(x = -EmittedC, y = ClimRegIndexAvgImpac, colour=HabAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+dev.off()
+
+
+## biodiv and regclim on the axes
+
+# efficiency frontier, no limit on third variable
+png(file = "eff-frontier-5.png",width=10,height=10,units="in",res=150)
+# make plot
+ggplot(dat, aes(x = HabAvgImpac, y = ClimRegIndexAvgImpac, colour=-EmittedC)) + geom_point() + scale_colour_gradient(low = "blue", high="red")
+dev.off()
+
+# reverse axes so shape is concave
+png(file = "eff-frontier-6.png",width=10,height=10,units="in",res=150)
+# make plot
+ggplot(dat, aes(x = HabAvgImpac, y = ClimRegIndexAvgImpac, colour=-EmittedC)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+dev.off()
+
+
+## all on one plot
+
+effa <- ggplot(dat, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+
+effb <- ggplot(dat, aes(x = -EmittedC, y = ClimRegIndexAvgImpac, colour=HabAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+
+effc <- ggplot(dat, aes(x = HabAvgImpac, y = ClimRegIndexAvgImpac, colour=-EmittedC)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+
+png(file = "eff-frontier-7.png",width=20,height=6,units="in",res=150) # tiff(file=...) also an option
+grid.arrange(effa, effb, effc, ncol=3)
+dev.off()
+
 
 
 ##### THIRD: True efficiency frontier with bin of impact for third dim
@@ -42,109 +101,57 @@ ggplot(dat, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + 
 
 
 # subset the data
-datsub <- subset(dat, dat$ClimRegIndexAvgImpac < 0.6 & dat$ClimRegIndexAvgImpac >= 0.56)
+datsub <- subset(dat, dat$HabAvgImpac < 600 & dat$HabAvgImpac >= 550)
 
 # plot 2-d scatter
-ggplot(datsub, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red")
+
+png(file = "eff-frontier-8.png",width=10,height=10,units="in",res=150) # tiff(file=...) also an option
+datsub <- subset(dat, dat$ClimRegIndexAvgImpac < 1.0 & dat$ClimRegIndexAvgImpac >= 0.58)
+ggplot(datsub, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+dev.off()
+
+png(file = "eff-frontier-9.png",width=10,height=10,units="in",res=150) # tiff(file=...) also an option
+datsub <- subset(dat, dat$ClimRegIndexAvgImpac < 1.0 & dat$ClimRegIndexAvgImpac >= 0.50)
+ggplot(datsub, aes(x = -EmittedC, y = HabAvgImpac, colour=ClimRegIndexAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+dev.off()
+
+datsub <- subset(dat, dat$HabAvgImpac < 620 & dat$HabAvgImpac >= 600)
+ggplot(datsub, aes(x = -EmittedC, y = ClimRegIndexAvgImpac, colour=HabAvgImpac)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
+
+datsub <- subset(dat, dat$EmittedC < 750 & dat$EmittedC >= 620)
+ggplot(dat, aes(x = HabAvgImpac, y = ClimRegIndexAvgImpac, colour=-EmittedC)) + geom_point() + scale_colour_gradient(low = "blue", high="red") + scale_y_reverse() + scale_x_reverse()
 
 
 
 
-##### SECOND: 3-D FIGURES WHERE TWO PRIORITIES VARY
-# from email to Steve: (subj: finally! figure!)
-# It kind of feels like you can only do a pairwise comparison in which two services vary and the third one is held equal, then watch how a single impact changes over the "priority space."  i.e. x-axis = habitat priority, y-axis = regional climate priority, z-axis = impact for whatever environmental outcome, and the entire figure is for carbon priority being held steady at 0.35 or whatever number.  I will try that out after lab meeting.
-
-# simple example
-# rr <- scatterplot3d(wCl,wC,EmittedC, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-# kind of hard to tell the third angle
-
-par(mfrow=c(3,3),oma=c(0,0,2,0),mar = c(5.1, 4.1, 2.1, 2.1))
-
-scatterplot3d(wC,wB,EmittedC, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-scatterplot3d(wC,wB,HabAvgImpac, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-scatterplot3d(wC,wB,ClimRegIndexAvgImpac, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-
-scatterplot3d(wC,wCl,EmittedC, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-scatterplot3d(wC,wCl,HabAvgImpac, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-scatterplot3d(wC,wCl,ClimRegIndexAvgImpac, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-
-scatterplot3d(wB,wCl,EmittedC, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-scatterplot3d(wB,wCl,HabAvgImpac, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-scatterplot3d(wB,wCl,ClimRegIndexAvgImpac, angle = 24, xlim = c(0, 1.0), ylim = c(0, 1.0))
-
-# ug, terrible looking
-
-
-
-##### THIRD: 3-D ENVIRONMENTAL EFFICIENCY FRONTIER
-
-# scatterplot3d
-rr <- scatterplot3d(EmittedC,HabAvgImpac,ClimRegIndexAvgImpac, angle = 24)
-
-# scatter3D
-scatter3D(x = EmittedC, y = HabAvgImpac, z = ClimRegIndexAvgImpac)
-# or
-rr <- scatter3D(x = EmittedC, y = HabAvgImpac, z = ClimRegIndexAvgImpac, 
-                colvar = ClimRegIndexAvgImpac, 
-                pch = 16, cex = 0.75, 
-                xlab = "EmittedC", ylab = "HabAvgImpac", 
-                zlab = "ClimRegIndexAvgImpac", clab = c("ClimRegIndexAvgImpac"),
-                ticktype = "detailed", nticks=3,
-                main = "3-D Eff Frontier",
-                d=35, theta=24,
-                colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75))
-
-
-
-
-# making the 3-D graph where the dots get thrown on the walls as well
-
-panelfirst <- function(pmat) {
-  zmin <- min(ClimRegIndexAvgImpac)
-  XY <- trans3D(EmittedC, HabAvgImpac, 
-                z = rep(zmin, nrow(dat)), pmat = pmat)
-  scatter2D(XY$x, XY$y, colvar = ClimRegIndexAvgImpac, pch = ".", 
-            cex = 3, add = TRUE, colkey = FALSE)
-  
-  xmin <- min(EmittedC)
-  XY <- trans3D(x = rep(xmin, nrow(dat)), y = HabAvgImpac, 
-                z = ClimRegIndexAvgImpac, pmat = pmat)
-  
-  scatter2D(XY$x, XY$y, colvar = ClimRegIndexAvgImpac, pch = ".", 
-            cex = 3, add = TRUE, colkey = FALSE)
-}
-
-pp <- with(dat, scatter3D(x = EmittedC, y = HabAvgImpac, z = ClimRegIndexAvgImpac, 
-                       colvar = ClimRegIndexAvgImpac, 
-                       pch = 16, cex = 1.2, xlab = "EmittedC", ylab = "HabAvgImpac", 
-                       zlab = "ClimRegIndexAvgImpac", clab = c("ClimReg"),
-                       main = "3-D Eff Frontier", ticktype = "detailed", 
-                       panel.first = panelfirst, theta = 30, phi=15, d = 2, 
-                       colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75)))
-
+##### FOURTH: 3-D examples that look ok
 
 ## all three shadows are present
 
-panelfirst <- function(pmat) {
-  zmin <- min(ClimRegIndexAvgImpac)
-  XY <- trans3D(EmittedC, HabAvgImpac, 
-                z = rep(zmin, nrow(dat)), pmat = pmat)
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-  xmin <- min(EmittedC)
-  XY <- trans3D(x = rep(xmin, nrow(dat)), y = HabAvgImpac, 
-                z = ClimRegIndexAvgImpac, pmat = pmat)
-  
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
+attach(dat)
 
-  ymax <- max(HabAvgImpac)
-  XY <- trans3D(EmittedC, y = rep(ymax, nrow(dat)),
-                z = ClimRegIndexAvgImpac, pmat = pmat)
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
+png(file = "3D-frontier-1.png",width=10,height=10,units="in",res=150) # tiff(file=...) also an option
+
+panelfirst <- function(pmat) {
+      zmin <- min(ClimRegIndexAvgImpac)
+      XY <- trans3D(EmittedC, HabAvgImpac, 
+                    z = rep(zmin, nrow(dat)), pmat = pmat)
+      scatter2D(XY$x, XY$y, pch = ".", 
+                cex = 5, col="#999999", add = TRUE, colkey = FALSE)
+      
+      xmin <- min(EmittedC)
+      XY <- trans3D(x = rep(xmin, nrow(dat)), y = HabAvgImpac, 
+                    z = ClimRegIndexAvgImpac, pmat = pmat)
+      
+      scatter2D(XY$x, XY$y, pch = ".", 
+                cex = 5, col="#999999", add = TRUE, colkey = FALSE)
+      
+      ymax <- max(HabAvgImpac)
+      XY <- trans3D(EmittedC, y = rep(ymax, nrow(dat)),
+                    z = ClimRegIndexAvgImpac, pmat = pmat)
+      scatter2D(XY$x, XY$y, pch = ".", 
+                cex = 5, col="#999999", add = TRUE, colkey = FALSE)
+      
 }
 
 pp <- with(dat, scatter3D(x = EmittedC, y = HabAvgImpac, z = ClimRegIndexAvgImpac, 
@@ -155,259 +162,48 @@ pp <- with(dat, scatter3D(x = EmittedC, y = HabAvgImpac, z = ClimRegIndexAvgImpa
                           panel.first = panelfirst, theta = 30, phi=15, d = 2, 
                           colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75)))
 
+dev.off()
 
 
 
-# make plot
-par(mfrow=c(2,3))
-plot(ClimRegIndexAvgImpac,EmittedC)
-plot(HabAvgImpac,EmittedC)
-plot(ClimRegIndexAvgImpac,HabAvgImpac)
-# switch order so easy to imagine along the walls of the 3-D plot
-plot(EmittedC,ClimRegIndexAvgImpac)
-plot(EmittedC,HabAvgImpac)
-plot(HabAvgImpac,ClimRegIndexAvgImpac)
-
-
-
-# based on this plot3D example
-# 
-# panelfirst <- function(pmat) {
-#   zmin <- min(-quakes$depth)
-#   XY <- trans3D(quakes$long, quakes$lat, 
-#                 z = rep(zmin, nrow(quakes)), pmat = pmat)
-#   scatter2D(XY$x, XY$y, colvar = quakes$mag, pch = ".", 
-#             cex = 2, add = TRUE, colkey = FALSE)
-#   
-#   xmin <- min(quakes$long)
-#   XY <- trans3D(x = rep(xmin, nrow(quakes)), y = quakes$lat, 
-#                 z = -quakes$depth, pmat = pmat)
-#   
-#   scatter2D(XY$x, XY$y, colvar = quakes$mag, pch = ".", 
-#             cex = 2, add = TRUE, colkey = FALSE)
-# }
-# 
-# with(quakes, scatter3D(x = long, y = lat, z = -depth, colvar = mag, 
-#                        pch = 16, cex = 1.5, xlab = "longitude", ylab = "latitude", 
-#                        zlab = "depth, km", clab = c("Richter","Magnitude"),
-#                        main = "Earthquakes off Fiji", ticktype = "detailed", 
-#                        panel.first = panelfirst, theta = 10, d = 2, 
-#                        colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75)))
-# 
-
-
-
-
-
-##### THINGS THAT WORK
-
-### try creating loess 3-D surface
-surf.df = data.frame(x = dat$ClimRegIndexAvgImpac, y = dat$HabAvgImpac, z = dat$EmittedC)
-
-surf.loess = loess(z~x*y, data=surf.df,degree=2,span=0.25)
-
-surf.fit = expand.grid(list(x=seq(0.0,1,0.05),y=seq(550,1300,25)))
-z = predict(surf.loess, newdata = surf.fit)
-surf.fit$Height = as.numeric(z)
-
+##### loess 3-D surface
 require(lattice)
-wireframe(Height~x*y, data = surf.fit, xlab = "ClimRegIndexAvgImpac", ylab = "HabAvgImpac", zlab="EmittedC", main = "Surface fit", drape = TRUE, colorkey = TRUE, scales=list(arrows=FALSE,cex=.5,tick.number="10"))
 
-
-
-
-
-
-### droplines till the fitted surface
-
-# make easier dataframe to read in the code below
+# figure model stuff
 surf.df = data.frame(x = dat$ClimRegIndexAvgImpac, y = dat$HabAvgImpac, z = dat$EmittedC)
+surf.loess = loess(z~x*y, data=surf.df,degree=2,span=0.25)
+surf.fit = expand.grid(list(x=seq(0.0,1,0.05),y=seq(550,1300,25)))
+g = predict(surf.loess, newdata = surf.fit)
+surf.fit$Height = as.numeric(g)
 
-with (surf.df, {
-  
-  # linear regression
-  fit <- lm(z ~ x + y, data=surf.df)
-  
-  # predict values on regular xy grid
-  x.pred <- seq(0.0,1, length.out = 25)
-  y.pred <- seq(550,1300, length.out = 25)
-  xy <- expand.grid(x = x.pred, 
-                    y = y.pred)
-  
-  fit.pred <- matrix (nrow = 25, ncol = 25, 
-                      data = predict(fit, newdata = data.frame(xy), 
-                                     interval = "prediction"))
-  
-  # fitted points for droplines to surface
-  fitpoints <- predict(fit) 
-  
-  scatter3D(z = surf.df$z, x = surf.df$x, y = surf.df$y, pch = 16, cex = 0.8, 
-            theta = 20, phi = 30, ticktype = "detailed",
-            xlab = "xlab", ylab = "ylab", zlab = "zlab",  
-            surf = list(x = x.pred, y = y.pred, z = fit.pred,  
-                        border = "black", facets = NA, fit = fitpoints),
-            main = "fit???") # fit = fitpoints makes the droplines
-  
-})
+# figure
+png(file = "3D-frontier-2.png",width=10,height=10,units="in",res=150) # tiff(file=...) also an option
+wireframe(Height~x*y, data = surf.fit, xlab = "ClimRegIndexAvgImpac", ylab = "HabAvgImpac", zlab="EmittedC", main = "Surface fit", drape = TRUE, colorkey = TRUE, scales=list(arrows=FALSE, cex=.5, tick.number=10))
+dev.off()
 
 
-
-
-### droplines till the fitted surface, poly response surf
-
-# make easier dataframe to read in the code below
-surf.df = data.frame(x = dat$ClimRegIndexAvgImpac, y = dat$HabAvgImpac, z = dat$EmittedC)
-
-with (surf.df, {
-  
-  # linear regression
-  fit2 <- lm(z ~ poly(x, y, degree = 2), data=surf.df)  
-  
-  # predict values on regular xy grid
-  x.pred <- seq(0.0,1, length.out = 25)
-  y.pred <- seq(550,1300, length.out = 25)
-  xy <- expand.grid(x = x.pred, 
-                    y = y.pred)
-  
-  fit.pred <- matrix (nrow = 25, ncol = 25, 
-                      data = predict(fit2, newdata = data.frame(xy), 
-                                     interval = "prediction"))
-  
-  # fitted points for droplines to surface
-  fitpoints <- predict(fit2) 
-  
-  scatter3D(z = surf.df$z, x = surf.df$x, y = surf.df$y, pch = 16, cex = 0.8, 
-            theta = 20, phi = 30, ticktype = "detailed",
-            xlab = "xlab", ylab = "ylab", zlab = "zlab",  
-            surf = list(x = x.pred, y = y.pred, z = fit.pred,  
-                        border = "black", facets = NA, fit2 = fitpoints),
-            main = "fit???") # fit = fitpoints makes the droplines
-  
-})
-
-
-
-
-### using rgl
+### can manipulate on screen using rgl
 
 tmp.lm <- lm(EmittedC ~ ClimRegIndexAvgImpac + HabAvgImpac, data = dat)
 tmp2.lm <- lm(EmittedC ~ poly(ClimRegIndexAvgImpac, HabAvgImpac, degree = 2), data = dat)
 summary.lm(tmp.lm)
 summary.lm(tmp2.lm)
 
-newdat <- expand.grid(ClimRegIndexAvgImpac=seq(0,1,by=0.1),
-                      HabAvgImpac=seq(0,1500,by=100))
+newdat <- expand.grid(ClimRegIndexAvgImpac=seq(0.1,0.7,by=0.1),
+                      HabAvgImpac=seq(400,750,by=25))
 newdat$pp <- predict(tmp.lm,newdata=newdat)
 newdat$pp2 <- predict(tmp2.lm,newdata=newdat)
 
 library(rgl)
 with(dat,plot3d(ClimRegIndexAvgImpac,HabAvgImpac,EmittedC))
-with(newdat,surface3d(unique(ClimRegIndexAvgImpac),unique(HabAvgImpac),pp,
-                      alpha=0.3,front="line"))
-#rgl.snapshot("swiss.png")
+with(newdat,surface3d(unique(ClimRegIndexAvgImpac),unique(HabAvgImpac),pp,alpha=0.3,front="line"))
+
+rgl.postscript("persp3dd.pdf","pdf")
 
 
 
 
 
-### all three shadows are present
-### flexible so can easily switch around variables
-
-xlimset = c(-3.5e10,-1e10)
-ylimset = c(500,1600)
-zlimset = c(0.15,0.7)
-
-xvarset = EmittedC
-yvarset = HabAvgImpac
-zvarset = ClimRegIndexAvgImpac
-
-xlabset = "EmittedC"
-ylabset = "HabAvgImpac"
-zlabset = "ClimRegIndexAvgImpac"
-
-
-
-panelfirst <- function(pmat) {
-  zmin <- zlimset[1]
-  XY <- trans3D(xvarset, yvarset, 
-                z = rep(zmin, nrow(dat)), pmat = pmat)
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-  xmin <- xlimset[1]
-  XY <- trans3D(x = rep(xmin, nrow(dat)), y = yvarset, 
-                z = zvarset, pmat = pmat)
-  
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-  ymax <- ylimset[2]
-  XY <- trans3D(xvarset, y = rep(ymax, nrow(dat)),
-                z = zvarset, pmat = pmat)
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-}
-
-pp <- with(dat, scatter3D(x = xvarset, y = yvarset, z = zvarset, 
-                          colvar = zvarset, 
-                          zlim = zlimset, ylim = ylimset, xlim = xlimset,
-                          pch = 16, cex = 1.2, xlab = xlabset, ylab = ylabset, 
-                          zlab = zlabset, clab = zlabset,
-                          main = "3-D Eff Frontier", ticktype = "detailed", 
-                          panel.first = panelfirst, theta = 20, phi=15, d = 2, 
-                          colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75)))
-
-
-
-
-
-### all three shadows are present
-
-zlimset = c(-3.5e10,-1e10)
-ylimset = c(500,1600)
-xlimset = c(0.15,0.7)
-
-zvarset = EmittedC
-yvarset = HabAvgImpac
-xvarset = ClimRegIndexAvgImpac
-
-zlabset = "EmittedC"
-ylabset = "HabAvgImpac"
-xlabset = "ClimRegIndexAvgImpac"
-
-
-
-panelfirst <- function(pmat) {
-  zmin <- zlimset[1]
-  XY <- trans3D(xvarset, yvarset, 
-                z = rep(zmin, nrow(dat)), pmat = pmat)
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-  xmin <- xlimset[1]
-  XY <- trans3D(x = rep(xmin, nrow(dat)), y = yvarset, 
-                z = zvarset, pmat = pmat)
-  
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-  ymax <- ylimset[2]
-  XY <- trans3D(xvarset, y = rep(ymax, nrow(dat)),
-                z = zvarset, pmat = pmat)
-  scatter2D(XY$x, XY$y, pch = ".", 
-            cex = 5, col="#999999", add = TRUE, colkey = FALSE)
-  
-}
-
-pp <- with(dat, scatter3D(x = xvarset, y = yvarset, z = zvarset, 
-                          colvar = zvarset, 
-                          zlim = zlimset, ylim = ylimset, xlim = xlimset,
-                          pch = 16, cex = 1.2, xlab = xlabset, ylab = ylabset, 
-                          zlab = zlabset, clab = zlabset,
-                          main = "3-D Eff Frontier", ticktype = "detailed", 
-                          panel.first = panelfirst, theta = 15, phi=15, d = 2,
-                          colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75)))
 
 
 
